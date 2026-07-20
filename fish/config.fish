37e7636ca29fc6cf67b5c1b8531559314685ee93
Brent Garner <bgarner@gmail.com>
2026-07-20 17:41:41 -0600

    jcode stuff

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,8 @@ fish_add_path $ANDROID_HOME/emulator
 fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin
 
 starship init fish | source
+
+# Added by jcode installer
+if not contains "/home/bgarner/.local/bin" $PATH
+    set -gx PATH "/home/bgarner/.local/bin" $PATH
+end
